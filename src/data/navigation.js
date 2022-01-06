99bd2a8e0f216e9b8eb49df64883b7d48a5ff072
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-06 13:55:47 +0000

    Added ternary operator for routeName mapping

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,25 +1,12 @@
 const NAV_TITLES = ["Home", "About", "Location", "Expertise", "Portfolio", "Contact"];
 
 const NAV_DATA = [];
-const set = {};
 for(let i = 0; i < NAV_TITLES.length; i++) {
-    if(NAV_TITLES[i] === "Expertise") {
-        NAV_DATA.push({
-            "id": i,
-            "name": NAV_TITLES[i],
-            "routeName": NAV_TITLES[i].toLowerCase(),
-            "items": [
-                "Kitchens",
-                "Bathrooms"
-            ]
-        });
-    } else {
-        NAV_DATA.push({
-            "id": i,
-            "name": NAV_TITLES[i],
-            "routeName": NAV_TITLES[i].toLowerCase()
-        });
-    }
+    NAV_DATA.push({
+        "id": i,
+        "name": NAV_TITLES[i],
+        "routeName": (NAV_TITLES[i] === "Home") ? "/" : NAV_TITLES[i].toLowerCase()
+    });
 }
 
 export default NAV_DATA;
