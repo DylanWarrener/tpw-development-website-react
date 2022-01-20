77c89fb7df42a2e39f681509e28d5796b4f95f19
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-20 18:01:14 +0000

    Added textarea component

---
+++ src/components/form/elements/textarea/textarea.jsx
@@ -0,0 +1,13 @@
+// React
+import React from 'react';
+
+// Stylesheet
+import './textarea.scss';
+
+export default function CustomTextarea(props) {
+    return (
+        <textarea>
+            This is the textarea message
+        </textarea>
+    );
+}
