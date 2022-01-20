b6e97985d16e4b9d00f126c75791610c73b7db6c
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-20 10:11:07 +0000

    Imported social media folder

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,13 +1,23 @@
 // Default react import
 import React from 'react';
 
+// Contact component 
+import SocialMedia from '../social-media/social-media';
+
 // Style sheet 
 import './footer.scss';
 
 export default function Footer() {
     return (
         <div className="footer">
-            Footer
+            <section>
+                <i className='outer-chevron'>
+                    <i className="inner-chevron"></i>
+                </i>
+                <div className='footer-panel'>
+                    This is the footer
+                </div>
+            </section> 
         </div>
     );
 }
