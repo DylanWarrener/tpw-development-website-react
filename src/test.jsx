edc99f7c5377ff4288241919128ad2f4507a7cc3
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-20 10:08:06 +0000

    Testing dynamic storage of data

--- src/test.jsx
+++ src/test.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
 
-import NAVIGATION_DATA from './data/navigation';
+import './test.scss';
 
 export default function Test() {
-    const NAV_TITLES = ["Home", "About", "Location", "Expertise", "Portfolio", "Contact"];
-    const NAV_DATA = [];
-    
-    for(let i = 0; i < NAV_TITLES.length; i++) {
-        NAV_DATA.push({
-            "id": i,
-            "name": NAV_TITLES[i],
-            "routeName": (NAV_TITLES[i] === "Home") ? "/" : NAV_TITLES[i].toLowerCase()
-        });
-    }
-
-    console.log(NAV_DATA);
-
     return (
         <div>
-
+            <img 
+                id='home' 
+                className='animated-chevron' 
+                src='./assets/animated/chevron/chevron.svg'
+                alt='home'
+            >
+                chevron
+            </img>
+            <div className='main'>This can be left unchanged</div>
         </div>
     );
 }
