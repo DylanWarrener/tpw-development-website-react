a1432c52f42dbdc8a94e47406655c23e45b2ee0c
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-24 16:27:54 +0000

    Added default structure to functional component

---
+++ src/pages/home-extension/home-extension.jsx
@@ -0,0 +1,13 @@
+// Default react
+import React from 'react';
+
+// Stylesheet
+import './home-extension.scss';
+
+export default function ExtensionPage() {
+    return (
+        <div>
+            Home extension page
+        </div>
+    );
+}
