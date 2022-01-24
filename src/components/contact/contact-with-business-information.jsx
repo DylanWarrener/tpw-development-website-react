265db109cf187b045cd89bedde426cb8616146af
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-24 16:20:32 +0000

    Added a subtitle

--- src/components/contact/contact-with-business-information.jsx
+++ src/components/contact/contact-with-business-information.jsx
@@ -18,32 +18,36 @@ class ContactFooterWithBusinessInformation extends Component {
     render() {
         return (
             <div id='contact-container'>
-                <form id='form-container'>
-                    <CustomFormInput 
-                        name='fullname'
-                        type='text'
-                        handleChange={this.handleChange}
-                        required
-                    />
-                    <CustomFormInput 
-                        name='email'
-                        type='email'
-                        handleChange={this.handleChange}
-                        required
-                    />
-                    <CustomFormInput 
-                        name='number'
-                        type='tel'
-                        handleChange={this.handleChange}
-                        pattern='[0-9]{11}'
-                        required
-                    />
-                    <CustomTextarea name='message' />
-                    <CustomSubmitButton type='submit'>SUBMIT</CustomSubmitButton>
-                </form>
-                <aside id='contact-information'>
-                    This is the contact information
-                </aside>
+                <h1 id='title'>CONTACT</h1>
+                <h3 id='subtitle'>Have a question or want to build a new home today?</h3>
+                <div id='contact-elements'>
+                    <form id='form-container'>
+                        <CustomFormInput 
+                            name='fullname'
+                            type='text'
+                            handleChange={this.handleChange}
+                            required
+                        />
+                        <CustomFormInput 
+                            name='email'
+                            type='email'
+                            handleChange={this.handleChange}
+                            required
+                        />
+                        <CustomFormInput 
+                            name='number'
+                            type='tel'
+                            handleChange={this.handleChange}
+                            pattern='[0-9]{11}'
+                            required
+                        />
+                        <CustomTextarea name='message' />
+                        <CustomSubmitButton type='submit'>SUBMIT</CustomSubmitButton>
+                    </form>
+                    <aside id='contact-information'>
+                        This is the contact information
+                    </aside>
+                </div>
             </div>
         );
     }
