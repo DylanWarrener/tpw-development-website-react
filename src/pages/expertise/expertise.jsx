91daf20ceb083d806c461e3a828c83ed05b9464b
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-24 16:27:32 +0000

    Removed files

--- src/pages/expertise/expertise.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Expertise(props) {
-    return (
-        <div>
-            Expertise
-        </div>
-    );
-}
--- src/pages/portfolio/portfolio.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Portfolio(props) {
-    return (
-        <div>
-            Portfolio
-        </div>
-    );
-}
