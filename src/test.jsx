5f031aaaac8650ec95df57c422eacfd4aa1f2da2
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-24 16:19:39 +0000

    Testing the navigation data with dictionaries

--- src/test.jsx
+++ src/test.jsx
@@ -3,17 +3,78 @@ import React from 'react';
 import './test.scss';
 
 export default function Test() {
+
+    const NAVIGATION_DATA = {}
+
+    const NAV_TITLES = [
+        "home", 
+        "kitchens", 
+        "bathrooms", 
+        "newbuilds", 
+        "extensions", 
+        "refurbishments",
+        "about",
+        "contact"
+    ];
+    
+    NAV_TITLES.forEach((title, index) => {
+        NAVIGATION_DATA[title] = {
+            "id": index,
+            "title": title.toUpperCase(),
+            "routeName": (title === "home") ? "/" : title.toLowerCase()
+        }
+    });
+    
+    console.log("The dictionary ", NAVIGATION_DATA);
+
+    Object.keys(NAVIGATION_DATA).map((key, index) => {
+        console.log("Title: ", NAVIGATION_DATA[key].title);
+    })
+
+    console.log(NAVIGATION_DATA.home.routeName);
+
+    /* 
+    const NAV_DATA_DICT = {
+        "Home": {
+            "id": 0,
+            "title": 'HOME',
+            "routeName": 'home'
+        }
+    }; 
+    */
+
+    /* 
+    const NAV_DATA_ARRAY = [
+        { "Home": {
+            "id": 0,
+            "title": 'HOME',
+            "routeName": 'home'
+        }}
+    ]; 
+    */
+
+    //console.log("Nav array: " + NAV_DATA_ARRAY);
+    //console.log("Nav dictionary: " + NAV_DATA_DICT.home);
+
+    /* NAV_DATA_ARRAY.push(title[{
+        "id": index,
+        "title": title.toUpperCase(),
+        "routeName": title.toLowerCase()
+    }]) */
+
     return (
         <div>
-            <img 
-                id='home' 
-                className='animated-chevron' 
-                src='./assets/animated/chevron/chevron.svg'
-                alt='home'
-            >
-                chevron
-            </img>
-            <div className='main'>This can be left unchanged</div>
+            Test
         </div>
     );
 }
+
+/* 
+ const ARRAY = [
+     'Home': {
+         id: 0,
+         title: 'Home'
+         routeName: 'home'
+     }
+ ]
+*/
