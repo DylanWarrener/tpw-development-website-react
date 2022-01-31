9a44a911dc96c92d1ccc7dc8a53416bf39c455b5
Dylan Warrener <dryanwarrener@gmail.com>
2022-01-31 16:18:34 +0000

    Updated imports

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,9 +1,32 @@
+// Socials
 import Facebook from './static/socials/Facebook.svg';
 import Whatsapp from './static/socials/Whatsapp.svg';
+
+// Logo
 import Logo from './static/logo/logo.svg';
+
+// Icons black color
+import PhoneBlack from './static/icons/phone-black.svg';
+import EmailBlack from './static/icons/email-black.svg';
+import TimeBlack from './static/icons/time-black.svg';
+import LocationBlack from './static/icons/location-black.svg';
+
+// Icons accent
+import PhoneAccent from './static/icons/phone-accent.svg';
+import EmailAccent from './static/icons/email-accent.svg';
+import TimeAccent from './static/icons/time-accent.svg';
+import LocationAccent from './static/icons/location-accent.svg';
 
 export {
     Facebook,
     Whatsapp,
-    Logo
+    Logo,
+    PhoneBlack,
+    EmailBlack,
+    TimeBlack,
+    LocationBlack,
+    PhoneAccent,
+    EmailAccent,
+    TimeAccent,
+    LocationAccent
 };
