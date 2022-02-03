5191caa3c5d310b0b9647e9925f304387cc637d7
Dylan Warrener <dryanwarrener@gmail.com>
2022-02-03 09:45:46 +0000

    Added btn functionailty for arrow icon

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,14 +5,29 @@ import React from 'react';
 import ContactWithBusinessInformation from '../contact/contact-with-business-information';
 import SocialsFooter from '../social-media/footer/socials-footer';
 
+// Icon 
+import { ReactComponent as ArrowUp } from '../../assets/static/icons/arrow-up.svg';
+
 // Style sheet 
 import './footer.scss';
 
-const Footer = () => {
+const Footer = ({ socials }) => {
+
+    const backToTop = () => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    };
+
     return (
         <div id="footer-container">
             <ContactWithBusinessInformation />
-            <SocialsFooter />
+            <button id='back-to-top-container' onClick={backToTop}>
+                <ArrowUp id='up-arrow'/>
+            </button>
+            <SocialsFooter socials={socials} />
         </div>
     );
 }
