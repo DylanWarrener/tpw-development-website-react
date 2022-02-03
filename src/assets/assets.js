235f5658c4baf1ca57301f35d872b247d0d24b72
Dylan Warrener <dryanwarrener@gmail.com>
2022-02-03 09:44:43 +0000

    Made a dictionary of assets

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -17,16 +17,72 @@ import EmailAccent from './static/icons/email-accent.svg';
 import TimeAccent from './static/icons/time-accent.svg';
 import LocationAccent from './static/icons/location-accent.svg';
 
-export {
-    Facebook,
-    Whatsapp,
-    Logo,
-    PhoneBlack,
-    EmailBlack,
-    TimeBlack,
-    LocationBlack,
-    PhoneAccent,
-    EmailAccent,
-    TimeAccent,
-    LocationAccent
+// Icons transparent 
+import ArrowUp from './static/icons/arrow-up.svg';
+
+// Images
+import HomeLandingPage from './static/temp/home.jpg';
+
+import KitchenLandingPage from './static/temp/kitchen.jpg';
+import KitchenFirstJob from './static/temp/first-job.jpg';
+
+import BathroomLandingPage from './static/temp/bathroom.jpg';
+
+import NewBuildLandingPage from './static/temp/newbuild.jpg';
+
+import ExtensionLandingPage from './static/temp/extension.jpg';
+
+import RefurbishmentLandingPage from './static/temp/refurbishment.jpg';
+
+import AboutLandingPage from './static/temp/about.jpg';
+
+const assetsObject = {
+    socials: {
+        facebook: Facebook,
+        whatsapp: Whatsapp
+    },
+    logo: Logo,
+    icons: {
+        black: {
+            phone: PhoneBlack,
+            email: EmailBlack,
+            time: TimeBlack,
+            location: LocationBlack
+        },
+        normal: {
+            phone: PhoneAccent,
+            email: EmailAccent,
+            time: TimeAccent,
+            location: LocationAccent
+        },
+        transparent: {
+            arrowUp: ArrowUp
+        }
+    }, 
+    images: {
+        home: {
+            landingPage: HomeLandingPage
+        },
+        kitchen: {
+            landingPage: KitchenLandingPage,
+            firstJob: KitchenFirstJob
+        },
+        bathroom: {
+            landingPage: BathroomLandingPage
+        },
+        newbuild: {
+            landingPage: NewBuildLandingPage
+        },
+        extension: {
+            landingPage: ExtensionLandingPage
+        },
+        refurbishment: {
+            landingPage: RefurbishmentLandingPage
+        },
+        about: {
+            landingPage: AboutLandingPage
+        }
+    }
 };
+
+export default assetsObject;
